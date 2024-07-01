13fec527ededcfe418adedd15074d03687d2035a
OmarCarranza15 <79736446+OmarCarranza15@users.noreply.github.com>
2024-07-01 15:35:04 -0600

    Actualizar razonsocial.jsx

--- razonsocial.jsx
+++ razonsocial.jsx
@@ -1,251 +1,32 @@
 import React, { useState, useEffect } from "react";
-import DataTable from "react-data-table-component";
-import Sidebar from "./SideNavBar.jsx"; // Ajusta la ruta según la ubicación real de Sidebar.jsx
-import TopBar from "./TopBar.jsx"; // Ajusta la ruta según la ubicación real de TopBar.jsx
-import styled from "styled-components";
-import "../styles/DataTable.css"; // Importa el archivo CSS
 import axios from "axios";
-import { FaEdit, FaSave, FaTimes } from "react-icons/fa"; // Importa el ícono de edición
-
-
-
-const MainContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-`;
-
-const ContentContainer = styled.div`
-  display: flex;
-  flex: 1;
-`;
-
-const HeaderContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1px 10px;
-`;
-
-const Title = styled.h2`
-  flex-grow: 1;
-  text-align: center;
-`;
-
-const FilterWrapper = styled.div`
-  position: relative; /* Cambiado de 'absolute' a 'relative' */
-  right: 9px;
-  padding: 10px;
-  background-color: #ffffff;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  z-index: 100;
-  display: ${(props) => (props.show ? "flex" : "none")};
-  flex-wrap: wrap;
-  margin: 0% 30%; 
-`;
-
-const FilterInput = styled.input`
-  padding: 8px;
-  margin-right: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-  font-size: 14px;
-  justify-content: center;
-  flex: 1;
-  display: flex; 
-`;
-
-const ButtonGroup = styled.div`
-  display: flex;
-  gap: 10px;
-`;
-
-//Estilos de Boton de Buscar y de Insertar un nuevo perfil
-const Button = styled.button`
-  background-color: ${(props) => (props.primary ? "#008cba" : "#4caf50")};
-  color: #ffffff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  gap: 5px;
-`;
-
-//Estilos del Boton cancelar
-const ButtonCancelar = styled.button`
-  background-color: ${(props) =>
-    props.primary ? "#008cba" : props.cancel ? "#bf1515" : "#4caf50"};
-  color: #ffffff;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  gap: 5px;
-`;
-
-const DataTableContainer = styled.div`
-  flex: 1;
-  padding: 1px;
-  overflow: auto;
-  position: relative;
-  width: 100%;
-  height: calc(100vh - 80px); /* Ajusta el tamaño del contenedor de la tabla */
-`;
-
-const StyledDataTable = styled(DataTable)`
-  border-collapse: collapse;
-  width: 40%;
-  position: relative;
-  margin: center;
-  margin: 0% 30%;
-
-  th,
-  td {
-    border: 1px solid #ddd;
-    padding: 15px;
-    text-align: center; /* Centra el texto en las celdas */
-    white-space: normal;
-    word-break: break-word;
-    overflow-wrap: break-word;
-  }
-
-  th {
-    background-color: #f0f0f0;
-    text-align: center; /* Centra el texto en los encabezados */
-  }
-
-  td {
-    min-width: 200px;
-    max-width: 500px;
-    text-align: center;
-  }
-
-  @media (max-width: 768px) {
-    th,
-    td {
-      padding: 8px;
-    }
-  }
-
-  @media (max-width: 480px) {
-    th,
-    td {
-      padding: 6px;
-    }
-  }
-`;
-
-const ModalBackground = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-`;
-
-const ModalWrapper = styled.div`
-  background-color: #ffffff;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
-  z-index: 1100;
-  animation: fadeIn 0.3s ease-out;
-  max-width: 400px;
-  width: 100%;
-
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-      transform: translateY(-20px);
-    }
-    to {
-      opacity: 1;
-      transform: translateY(0);
-    }
-  }
-`;
-
-const ModalTitle = styled.h3`
-  margin-bottom: 15px;
-  text-align: center;
-`;
-
-const ModalInput = styled.input`
-  width: 100%;
-  padding: 12px;
-  margin-bottom: 15px;
-  border: 1px solid ${(props) => (props.error ? "red" : "#ccc")};
-  border-radius: 5px;
-  font-size: 16px;
-  outline: none;
-  box-sizing: border-box;
-  transition: border-color 0.3s ease;
-
-  &:focus {
-    border-color: #008cba;
-    box-shadow: 0 0 5px rgba(0, 140, 186, 0.5);
-  }
-`;
-
-const ErrorMessage = styled.p`
-  color: red;
-  font-size: 12px;
-  margin-bottom: 10px;
-`;
-
-const ModalButtonGroup = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  gap: 10px;
-`;
-
-const ModalButton = styled.button`
-  background-color: ${(props) =>
-    props.primary ? "#4caf50" : props.cancel ? "#bf1515" : "#4caf50"};
-  color: #ffffff;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 5px;
-  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: ${(props) =>
-      props.primary ? "#45a049" : props.cancel ? "#ad1111" : "#45a049"};
-  }
-`;
-
-const SelectPais = styled.select`
-  width: 100%;
-  padding: 7px;
-  margin-bottom: 9px;
-  border: 2px solid #ccc;
-  border-radius: 4px;
-  font-size: 14px;
-  ${(props) => props.error && `border: 1px solid red;`}
-`;
-
-
+import { FaSave, FaEdit, FaTimes } from "react-icons/fa";
+import {
+  MainContainer,
+  TopBar,
+  ContentContainer,
+  Sidebar,
+  DataTableContainer,
+  HeaderContainer,
+  Title,
+  ButtonGroup,
+  Button,
+  FilterWrapper,
+  FilterInput,
+  StyledDataTable,
+  ModalBackground,
+  ModalWrapper,
+  ModalTitle,
+  SelectPais,
+  ModalInput,
+  ErrorMessage,
+  ModalButtonGroup,
+  ModalButton,
+  ButtonCancelar
+} from "./StyledComponents"; // Asegúrate de que las rutas son correctas
 
 function RazonSocial() {
-  const [rsocial] = useState([]);
+  const [rsocial, setRSocial] = useState([]);
   const [records, setRecords] = useState([]);
   const [showFilters, setShowFilters] = useState(false);
   const [editMode, setEditMode] = useState(false);
@@ -323,7 +104,7 @@ function RazonSocial() {
     setModalValues((prevValues) => ({ ...prevValues, [field]: value }));
   };
 
-  const SaveModal = () => {
+  const SaveModal = async () => {
     const newErrors = { pais: "", rsocial: "" };
 
     if (!modalValues.pais.trim()) {
@@ -342,19 +123,41 @@ function RazonSocial() {
     setErrors(newErrors);
 
     if (Object.values(newErrors).every((error) => error === "")) {
-      // Ingresar una nueva razón social
-      console.log("Nueva Razón Social:", modalValues.rsocial);
-      const updatedRecords = [
-        ...records,
-        {
-          id: records.length + 1,
-          pais: modalValues.pais,
-          rsocial: modalValues.rsocial,
-        },
-      ];
-      setRecords(updatedRecords);
-      setShowModal(false); // Ocultar el modal después de guardar
-      setModalValues({ pais: "", rsocial: "" }); // Limpiar los valores del modal
+      try {
+        // Verificar si la razón social ya existe en la base de datos
+        const response = await axios.get(`http://localhost:3000/rsocial`);
+        const rsocialExists = response.data.some(
+          (rsocial) => rsocial.N_RSocial.toLowerCase() === modalValues.rsocial.toLowerCase()
+        );
+        if (rsocialExists) {
+          setErrors({ rsocial: "La razón social ya existe" });
+          return;
+        }
+
+        // Insertar una nueva razón social
+        const newRSocial = {
+          N_RSocial: modalValues.rsocial,
+          ID_Pais: modalValues.pais,
+        };
+        const insertResponse = await axios.post(`http://localhost:3000/rsocial`, newRSocial);
+
+        // Actualizar la lista de razones sociales con la nueva razón social
+        const paisResponse = await axios.get(`http://localhost:3000/pais/${modalValues.pais}`);
+        const updatedRecords = [
+          ...records,
+          {
+            id: insertResponse.data.id,
+            N_RSocial: modalValues.rsocial,
+            ID_Pais: modalValues.pais,
+            N_Pais: paisResponse.data.N_Pais,
+          },
+        ];
+        setRecords(updatedRecords);
+        setShowModal(false); // Ocultar el modal después de guardar
+        setModalValues({ pais: "", rsocial: "" }); // Limpiar los valores del modal
+      } catch (error) {
+        console.error("Error al insertar la nueva Razón Social:", error);
+      }
     }
   };
 
@@ -497,85 +300,44 @@ function RazonSocial() {
         ),
     },
   ];
+  {/* Modal para insertar una nueva razón social */}
+  {showModal && (
+    <ModalBackground>
+      <ModalWrapper>
+        <ModalTitle>Nueva Razón Social</ModalTitle>
+        <SelectPais
+          value={modalValues.ID_Pais}
+          onChange={(e) => handleModalChange(e, "ID_Pais")}
+          placeholder="País"
+          error={errors.pais}
+          required
+        >
+          <option value="">Seleccione un país</option>
+          {pais.map((p) => (
+            <option key={p.id} value={p.id}>
+              {p.N_Pais}
+            </option>
+          ))}
+        </SelectPais>
+        {errors.pais && <ErrorMessage>{errors.pais}</ErrorMessage>}
 
-  return (
-    <MainContainer>
-      <TopBar />
-      <ContentContainer>
-        <Sidebar />
-        <DataTableContainer>
-          <HeaderContainer>
-            <Title><h2 className="Title">Razones Sociales</h2></Title>
-            <ButtonGroup>
-              <Button primary onClick={toggleFilters}>
-                {showFilters ? "Ocultar" : "Buscar"}
-              </Button>
-              <Button onClick={handleInsert}>Nueva Razon Social</Button>
-            </ButtonGroup>
-          </HeaderContainer>
-          <FilterWrapper show={showFilters}>
-            <FilterInput
-              type="text"
-              value={filters.N_Pais}
-              onChange={(e) => handleFilterChange(e, "N_Pais")}
-              placeholder="Buscar por Pais"
-            />
-            <FilterInput
-              type="text"
-              value={filters.N_RSocial}
-              onChange={(e) => handleFilterChange(e, "N_RSocial")}
-              placeholder="Buscar por Razon Social"
-            />
-          </FilterWrapper>
-          <StyledDataTable
-            columns={columns}
-            data={filteredData}
-            pagination
-            paginationPerPage={30}
-            showFilters={showFilters}
-          />
-        </DataTableContainer>
-      </ContentContainer>
-
-      {/* Modal para insertar una nueva razón social */}
-      {showModal && (
-        <ModalBackground>
-          <ModalWrapper>
-            <ModalTitle>Nueva Razón Social</ModalTitle>
-            <SelectPais
-              value={modalValues.ID_Pais}
-              onChange={(e) => handleModalChange(e, "ID_Pais")}
-              placeholder="País"
-              error={errors.pais}
-              required
-            >
-              <option value="">Seleccione un país</option>
-            //aqui se debe generar los paises para hacer la insercion 
-          
-            </SelectPais>
-            {errors.pais && <ErrorMessage>{errors.pais}</ErrorMessage>}
-
-            <ModalInput
-              type="text"
-              value={modalValues.rsocial}
-              onChange={(e) => handleModalChange(e, "rsocial")}
-              placeholder="Razón Social"
-              error={errors.rsocial}
-            />
-            {errors.rsocial && <ErrorMessage>{errors.rsocial}</ErrorMessage>}
-            <ModalButtonGroup>
-              <ModalButton primary onClick={SaveModal}>
-                <FaSave /> Guardar
-              </ModalButton>
-              <ModalButton cancel onClick={handleCloseModal}>
-                <FaTimes /> Cancelar
-              </ModalButton>
-            </ModalButtonGroup>
-          </ModalWrapper>
-        </ModalBackground>
-      )}
-    </MainContainer>
-  );
-}
-
-export default RazonSocial;
+        <ModalInput
+          type="text"
+          value={modalValues.rsocial}
+          onChange={(e) => handleModalChange(e, "rsocial")}
+          placeholder="Razón Social"
+          error={errors.rsocial}
+        />
+        {errors.rsocial && <ErrorMessage>{errors.rsocial}</ErrorMessage>}
+        <ModalButtonGroup>
+          <ModalButton primary onClick={SaveModal}>
+            <FaSave /> Guardar
+          </ModalButton>
+          <ModalButton cancel onClick={handleCloseModal}>
+            <FaTimes /> Cancelar
+          </ModalButton>
+        </ModalButtonGroup>
+      </ModalWrapper>
+    </ModalBackground>
+  )}
+</MainContainer>
