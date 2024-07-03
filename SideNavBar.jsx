6a4acb108d3711185b04fa287af2d99932ab5a7f
OmarCarranza15 <79736446+OmarCarranza15@users.noreply.github.com>
2024-07-03 16:14:51 -0600

    Update SideNavBar.jsx

--- SideNavBar.jsx
+++ SideNavBar.jsx
@@ -28,6 +28,7 @@ const SideNavBar = () => {
             subNav: paises.map( pais=> ({
                 title: pais.N_Pais,
                 icon: <GrFlag/>, // Icono de bandera
+                path:"/landingPage"
             }
             ))
             
