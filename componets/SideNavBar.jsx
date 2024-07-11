363100c4188b181c32edbc014763e34e1e5cd9d0
OmarCarranza15 <79736446+OmarCarranza15@users.noreply.github.com>
2024-07-11 11:20:35 -0600

    Update SideNavBar.jsx

--- componets/SideNavBar.jsx
+++ componets/SideNavBar.jsx
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { GrTable, GrSystem, GrUserSettings, GrGroup, GrGlobe, GrContactInfo, GrMonitor, GrOrganization, GrShieldSecurity, GrMultiple, GrFolder, GrSearch, GrArticle, GrBusinessService } from "react-icons/gr";
+import {
+  GrTable,
+  GrSystem,
+  GrUserSettings,
+  GrGroup,
+  GrGlobe,
+  GrBusinessService,
+  GrContactInfo,
+  GrMonitor,
+  GrOrganization,
+  GrShieldSecurity,
+  GrMultiple,
+  GrFolder,
+  GrSearch,
+  GrDocumentDownload,
+} from "react-icons/gr";
+import { LiaUserTieSolid } from "react-icons/lia";
+import styled from "styled-components";
 import "../styles/SideNavBar.css";
 import axios from "axios";
-import styled from "styled-components";
 
 const Banderas = styled.img`
   width: 20px;
@@ -20,7 +36,7 @@ const SideNavBar = () => {
         const response = await axios.get(`http://localhost:3000/pais/`);
         setPaises(response.data);
       } catch (error) {
-        console.error('Error al obtener la lista de paises', error);
+        console.error("Error al obtener la lista de paises", error);
       }
     };
     fetchPaises();
@@ -30,30 +46,112 @@ const SideNavBar = () => {
     {
       title: "Matriz de Perfiles",
       icon: <GrTable />,
-      subNav: paises.map(pais => ({
-        title: pais.N_Pais,
-        icon: <Banderas src={require(`../imgs/${pais.N_Pais.toLowerCase()}.png`)} alt={`Bandera de ${pais.N_Pais}`} />, // Icono de bandera
-        path: `/matrizPais?pais=${encodeURIComponent(pais.N_Pais)}`
-      }))
+      subNav: [
+        {
+          title: "Reportes",
+          path: "/reportes",
+          icon: <GrDocumentDownload />, // Icono de reporte
+        },
+      ].concat(
+        paises.map((pais) => ({
+          title: pais.N_Pais,
+          icon: <Banderas src={require(`../imgs/${pais.N_Pais}.png`)} alt="Flag image" />,// Imagen de bandera
+          path: `/matrizPais?pais=${encodeURIComponent(pais.N_Pais)}&id=${pais.id}`,
+
+          //path:`/landingPage?pais=${pais.N_Pais}`
+        }))
+      ),
     },
-    // Otros ítems del menú...
+    {
+      title: "Estructura",
+      icon: <GrGroup />, // Icono de edificio
+      subNav: [
+        {
+          title: "Paises",
+          path: "/paises",
+          icon: <GrGlobe />, // Icono de globo
+        },
+        {
+          title: "División",
+          path: "/division",
+          icon: <GrBusinessService />,
+        },
+        {
+          title: "Razón Social",
+          path: "/razonsocial",
+          icon: <GrOrganization />, // Icono de empresa
+        },
+        {
+          title: "Departamentos",
+          path: "/departamento",
+          icon: <GrFolder />, // Icono de carpeta
+        },
+        {
+          title: "Aplicaciones",
+          path: "/aplicacion",
+          icon: <GrMonitor />, // Icono de aplicaciones
+        },
+        {
+          title: "Ambientes",
+          path: "/ambiente",
+          icon: <GrSystem />, // Icono de nube
+        },
+        {
+          title: "Centro de Costes",
+          path: "/centrocosto",
+          icon: <GrMultiple />, // Icono
+        },
+        {
+          title: "Puestos",
+          path: "/puesto",
+          icon: <LiaUserTieSolid />, // Icono de Puesto
+        },
+      ],
+    },
+    {
+      title: "Gestión de Usuarios",
+      icon: <GrUserSettings />, // Icono de usuario
+      subNav: [
+        {
+          title: "Usuarios",
+          path: "/usuario",
+          icon: <GrContactInfo />, // Icono de personas
+        },
+        {
+          title: "Roles y Permisos",
+          path: "/rolesypermisos",
+          icon: <GrShieldSecurity />, // Icono de candado
+        },
+        {
+          title: "Auditoría",
+          path: "/auditoria",
+          icon: <GrSearch />, // Icono de lupa
+        },
+      ],
+    },
   ];
 
   const toggleSubMenu = (index) => {
     setActiveMenu(activeMenu === index ? null : index);
   };
 
+  // Función para expandir el sidebar
   const expandSidebar = () => {
     setExpandState(true);
   };
 
+  // Función para contraer el sidebar
   const collapseSidebar = () => {
     setExpandState(false);
   };
 
   return (
     <div
-      className={isExpanded ? "side-nav-container" : "side-nav-container side-nav-container-NX"}
+      className={
+        isExpanded
+          ? "side-nav-container"
+          : "side-nav-container side-nav-container-NX"
+      }
       onMouseEnter={expandSidebar}
       onMouseLeave={collapseSidebar}
     >
@@ -89,8 +187,7 @@ const SideNavBar = () => {
       <div className="nav-footer">
         {isExpanded && (
           <div className="nav-details">
-            <div className="nav-footer-info">
-            </div>
+            <div className="nav-footer-info"></div>
           </div>
         )}
       </div>
