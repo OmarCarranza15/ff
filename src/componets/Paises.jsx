6ad560b6405a54ed6a388ff54d85ecdb52f36b74
OmarCarranza15 <79736446+OmarCarranza15@users.noreply.github.com>
2024-07-29 16:46:57 -0600

    Add files via upload

---
+++ src/componets/Paises.jsx
@@ -0,0 +1,564 @@
+
+import React, { useState, useEffect } from "react";
+import DataTable from "react-data-table-component";
+import Sidebar from "./SideNavBar.jsx"; // Ajusta la ruta según la ubicación real de Sidebar.jsx
+import TopBar from "./TopBar.jsx"; // Ajusta la ruta según la ubicación real de TopBar.jsx
+import styled from "styled-components";
+import "../styles/DataTable.css"; // Importa el archivo CSS
+import "../styles/error.css";
+import axios from "axios";
+import CryptoJS from "crypto-js";
+import { FaUndo, FaSearch, FaPlus } from "react-icons/fa"; // Importa los íconos necesarios
+import {
+  customStyles,
+  CustomLoader,
+  HeaderContainer,
+  Title,
+  MainContainer,
+  ContentContainer,
+  FilterWrapper,
+  FilterInput,
+  ButtonGroup,
+  Button,
+  StyledInput,
+  RedButton,
+  DataTableContainer,
+  ModalBackground,
+  ModalWrapper,
+  ModalTitle,
+  ErrorMessage,
+  ModalButtonGroup,
+} from "./Estilos.jsx";
+
+const StyledDataTable = styled(DataTable)`
+  border-collapse: collapse;
+  width: 30%;
+  position: relative;
+  margin: center;
+  margin: 1% 30%;
+
+  th,
+  td {
+    border: 1px solid #ddd;
+    padding: 15px;
+    text-align: center; /* Centra el texto en las celdas */
+    white-space: normal;
+    word-break: break-word;
+    overflow-wrap: break-word;
+  }
+
+  th {
+    background-color: #f0f0f0;
+    text-align: center; /* Centra el texto en los encabezados */
+  }
+
+  td {
+    min-width: 200px;
+    max-width: 500px;
+    text-align: center;
+  }
+
+  @media (max-width: 768px) {
+    th,
+    td {
+      padding: 8px;
+    }
+  }
+
+  @media (max-width: 480px) {
+    th,
+    td {
+      padding: 6px;
+    }
+  }
+`;
+
+const secretKey = 'mySecretKey'; // Clave secreta para desencriptar datos
+
+const decryptData = (encryptedData) => {
+  try {
+    const bytes = CryptoJS.AES.decrypt(encryptedData, secretKey);
+    return bytes.toString(CryptoJS.enc.Utf8).replace(/^"|"$/g, ''); // Eliminar comillas
+  } catch (error) {
+    console.error("Error al desencriptar datos:", error);
+    return '';
+  }
+};
+
+function Paises() {
+  const [pais] = useState([]);
+  const [records, setRecords] = useState([]);
+  const [showFilters, setShowFilters] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [editedRow, setEditedRow] = useState(null);
+  const [errors, setErrors] = useState({ pais: "" }); // validaciones para insertar un nuevo país
+  const [modalValues, setModalValues] = useState({ pais: "" });
+  const [showModal, setShowModal] = useState(false); // Estado para mostrar/ocultar el modal
+  const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState({
+    N_Pais: "",
+  });
+  const [rows, setRows] = useState([]);
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+
+    const encryptedUser = sessionStorage.getItem('userId');
+    if (encryptedUser) {
+        const decryptedUser = decryptData(encryptedUser);
+        //console.log('Rol desencriptado:', decryptedRole); // Consola para verificar el rol desencriptado
+        setUserId(decryptedUser);
+    }
+
+
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(`http://localhost:3000/pais/`);
+        const data = response.data;
+        const mappedData = data.map((pais) => ({
+          id: pais.id,
+          N_Pais: pais.N_Pais,
+        }));
+        setRecords(mappedData);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error al obtener los Paises:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleFilterChange = (event, column) => {
+    const { value } = event.target;
+    setFilters((prevFilters) => ({ ...prevFilters, [column]: value }));
+  };
+
+  const toggleFilters = () => {
+    setShowFilters(!showFilters);
+  };
+
+  const handleInsert = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setModalValues({ pais: "" });
+    setErrors({ pais: "" });
+  };
+
+  const handleModalChange = (event, field) => {
+    const { value } = event.target;
+    setModalValues((prevValues) => ({ ...prevValues, [field]: value }));
+  };
+
+  const SaveModal = async () => {
+    const newErrors = { pais: "" };
+
+    if (!modalValues.pais.trim()) {
+      newErrors.pais = "El campo País es obligatorio";
+    } else if (!/^[a-zA-ZÑñ\s]+$/.test(modalValues.pais)) {
+      newErrors.pais = "El campo País solo acepta letras y espacios en blanco";
+    }
+    setErrors(newErrors);
+
+    if (Object.values(newErrors).every((error) => error === "")) {
+      try {
+        // Verificar si el país ya existe en la base de datos
+        const response = await axios.get(
+          `http://localhost:3000/pais?N_Pais=${modalValues.pais}`
+        );
+        if (
+          response.data.some(
+            (pais) =>
+              pais.N_Pais.toLowerCase() === modalValues.pais.toLowerCase()
+          )
+        ) {
+          setErrors({ pais: "El país ya existe" });
+          return;
+        }
+
+        // Insertar un nuevo país
+        const newPais = { N_Pais: modalValues.pais };
+        const insertResponse = await axios.post(
+          `http://localhost:3000/pais`,
+          newPais
+        );
+
+        await axios.post('http://localhost:3000/auditoria', {
+          Tabla: `Paises`,
+          Accion: 1, // 1 for Insert
+          ID_Usuario: userId,
+          Fecha: new Date(),
+        });
+
+        // Actualizar la lista de países con el nuevo país
+        const updatedRecords = [
+          ...records,
+          { id: insertResponse.data.id, N_Pais: modalValues.pais },
+        ];
+        setRecords(updatedRecords);
+        setShowModal(false); // Ocultar el modal después de guardar
+        setModalValues({ pais: "" }); // Limpiar los valores del modal
+
+        // Estilo de notificacion
+        const toastElement = document.createElement("div");
+        toastElement.className = "toast-success";
+        toastElement.innerHTML = "¡Pais insertado correctamente!";
+
+        document.body.appendChild(toastElement);
+
+        // Ocultar la notificacion luego de 1 segundo
+        setTimeout(() => {
+          toastElement.remove();
+          window.location.reload(); // Recargar la pagina despues de 1 segundos
+        }, 1000);
+      } catch (error) {
+        console.error("Error al insertar el nuevo País:", error);
+      }
+    }
+  };
+
+  const startEdit = (row) => {
+    console.log("row object:", row);
+    setEditedRow({ ...row });
+    setEditMode(row.id);
+  };
+
+  const handleEditChange = (event, field) => {
+    const { value } = event.target;
+    setEditedRow((prevState) => ({
+      ...prevState,
+      [field]: value,
+      ...(field === "ID_Pais" && {
+        N_Pais: pais.find((p) => p.id === parseInt(value)).N_Pais,
+      }),
+    }));
+    validateInput(field, value); // Validación de los campos a editar
+  };
+
+  const validateInput = (field, value) => {
+    let newErrors = { ...errors };
+    if (field === "pais") {
+      if (!value.trim()) {
+        newErrors.pais = "El campo País es obligatorio";
+      } else if (!/^[a-zA-ZÑñ\s]+$/.test(value)) {
+        newErrors.pais =
+          "El campo País solo acepta letras y espacios en blanco";
+      } else {
+        newErrors.pais = "";
+      }
+    }
+    setErrors(newErrors);
+  };
+
+  const saveChanges = async (id) => {
+    try {
+      const updateRow = {
+        ...editedRow,
+      };
+
+      // Verificar si el país ya existe
+      if (editedRow && editedRow.N_Pais) {
+        console.log("Checking if country exists...");
+        const response = await axios.get(
+          `http://localhost:3000/pais?N_Pais=${editedRow.N_Pais}`
+        );
+
+        console.log("Response:", response.data);
+
+        if (
+          response.data.some(
+            (pais) =>
+              pais.N_Pais.toLowerCase() === editedRow.N_Pais.toLowerCase()
+          )
+        ) {
+          const errorNotification = document.createElement("div");
+          errorNotification.className = "error-notification";
+          errorNotification.innerHTML = `
+            <span class="error-icon">!</span>
+            <span class="error-message">
+              El País que intenta actualizar ya existe en la base de datos.
+              Por favor, ingrese una país diferente.
+            </span>
+          `;
+          document.body.appendChild(errorNotification);
+
+          // Ocultar la notificación después de 2 segundos
+          setTimeout(() => {
+            errorNotification.remove();
+          }, 2000);
+          return;
+        }
+      } else {
+        console.error("Error: editedRow.N_Pais is undefined or empty");
+        setErrors({ N_Pais: "El campo país no puede estar vacío" });
+        return;
+      }
+
+      // Check if the updated country is already registered
+      const existingCountry = records.find(
+        (row) => row.N_Pais.toLowerCase() === editedRow.N_Pais.toLowerCase()
+      );
+
+      if (existingCountry && existingCountry.id !== id) {
+        setErrors({ N_Pais: "El país ya está registrado" });
+        return;
+      }
+
+      await axios.put(`http://localhost:3000/pais/${id}`, updateRow);
+
+      await axios.post('http://localhost:3000/auditoria', {
+        Tabla: `Paises`,
+        Accion: 2, // 2 for Update
+        ID_Usuario: userId,
+        Fecha: new Date()
+        })
+
+      const updatedRecords = records.map((row) =>
+        row.id === id ? { ...editedRow } : row
+      );
+
+      setRecords(updatedRecords);
+      setEditedRow(null);
+      setEditMode(null);
+
+      // Notificación de datos actualizados
+      const toastElement = document.createElement("div");
+      toastElement.className = "toast-notification";
+      toastElement.innerHTML = "País actualizado con éxito!";
+
+      document.body.appendChild(toastElement);
+
+      // Ocultar la notificación después de 1 segundo
+      setTimeout(() => {
+        toastElement.remove();
+        window.location.reload(); // Recargar la pagina despues de 1 segundo
+      }, 1000);
+    } catch (error) {
+      console.error("Error al guardar los cambios", error);
+    }
+  };
+
+  const cancelEdit = () => {
+    setEditedRow(null);
+    setEditMode(null);
+  };
+
+  const filteredData = records.filter((row) => {
+    return (
+      filters.N_Pais === "" ||
+      row.N_Pais.toLowerCase().includes(filters.N_Pais.toLowerCase())
+    );
+  });
+
+  const deleteRow = (id) => {
+    axios
+      .get(`/Datosdependientes/${id}`)
+      .then((response) => {
+        if (response.data.hasDependencies) {
+          alert(
+            "No se puede eliminar este registro porque tiene dependencias en otros registros."
+          );
+        } else {
+          axios
+            .delete(`/delete/${id}`)
+            .then((response) => {
+              // Actualiza la lista de filas después de eliminar una fila
+              setRows(rows.filter((row) => row.id !== id));
+            })
+            .catch((error) => console.error(error));
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const columns = [
+    {
+      name: "País",
+      selector: (row) => row.id,
+      sortable: true,
+      minwidth: "200px", // Ajusta el tamaño mínimo según sea necesario
+      maxwidth: "50px", // Ajusta el tamaño máximo según sea necesario
+      cell: (row) =>
+        editMode && editedRow?.id === row.id ? (
+          <StyledInput
+            type="text"
+            value={editedRow.N_Pais}
+            onChange={(e) => handleEditChange(e, "N_Pais")}
+            onKeyPress={(e) => {
+              if (!/[a-zA-ZñÑáéíóúÁÉÍÓÚ]/.test(e.key)) {
+                e.preventDefault();
+              }
+            }}
+          />
+        ) : (
+          <div>{row.N_Pais}</div>
+        ),
+    },
+    {
+      name: "Acciones",
+      cell: (row) =>
+        editMode === row.id ? (
+          <ButtonGroup>
+            <Button
+              type="button"
+              className="btn btn-outline-success"
+              onClick={() => saveChanges(row.id)}
+            >
+              Guardar
+            </Button>
+            <Button
+              type="button"
+              className="btn btn-outline-danger"
+              onClick={cancelEdit}
+            >
+              Cancelar
+            </Button>
+          </ButtonGroup>
+        ) : (
+          <ButtonGroup>
+            <Button
+              type="button"
+              className="btn btn-outline-primary"
+              onClick={() => startEdit(row)}
+            >
+              Editar
+            </Button>
+            <Button
+              type="button"
+              className="btn btn-outline-danger"
+              onClick={() => {
+                if (
+                  window.confirm(
+                    "¿Estás seguro que deseas eliminar este registro?"
+                  )
+                ) {
+                  deleteRow(row.id);
+                }
+              }}
+            >
+              Eliminar
+            </Button>
+          </ButtonGroup>
+        ),
+    },
+  ];
+
+  const resetFilters = () => {
+    setFilters({
+      N_Pais: "",
+    });
+  };
+
+  return (
+    <MainContainer>
+      <TopBar />
+      <ContentContainer>
+        <Sidebar />
+        <DataTableContainer>
+          <HeaderContainer>
+            <Title>Paises</Title>
+            <ButtonGroup>
+              <Button
+                className="btn btn-outline-primary"
+                onClick={toggleFilters}
+              >
+                <FaSearch style={{margin: "0px 5px"}}/>
+                {showFilters ? "Ocultar" : "Buscar"}
+              </Button>
+              <Button
+                style={{marginRight: "30px"}}
+                className="btn btn-outline-success"
+                onClick={handleInsert}
+              >
+                <FaPlus style={{margin: "0px 5px"}}/>
+                Nuevo Pais
+              </Button>
+            </ButtonGroup>
+          </HeaderContainer>
+          <FilterWrapper show={showFilters}>
+            <FilterInput
+              type="text"
+              value={filters.N_Pais}
+              onChange={(e) => handleFilterChange(e, "N_Pais")}
+              placeholder="Buscar por Pais"
+            />
+            <ButtonGroup>
+              <RedButton onClick={resetFilters}>
+                <FaUndo /> Resetear Filtros
+              </RedButton>
+            </ButtonGroup>
+          </FilterWrapper>
+          {loading ? (
+            <CustomLoader />
+          ) : (
+            <StyledDataTable
+              columns={columns}
+              data={filteredData}
+              pagination
+              paginationPerPage={15}
+              customStyles={customStyles}
+            />
+          )}
+        </DataTableContainer>
+      </ContentContainer>
+      {/* Modal para insertar nuevo país */}
+      {showModal && (
+        <ModalBackground>
+          <ModalWrapper>
+            <ModalTitle>Insertar Nuevo País</ModalTitle>
+
+            <div style={{ margin: "15px" }} />
+            <label
+              style={{ width: "100%", display: "block", textAlign: "left" }}
+            >
+              <span style={{ textAlign: "left" }}>Nombre del País:</span>
+            </label>
+            <input
+              className="form-control"
+              type="text"
+              value={modalValues.pais}
+              onInput={(e) => {
+                const value = e.target.value;
+                const newValue = value.toLowerCase().replace(/(^\w|\s\w)/g, (match) => match.toUpperCase());
+                e.target.value = newValue;
+                handleModalChange(e, "pais");
+              }}
+              placeholder="Ingrese el nombre"
+              error={errors.pais}
+              pattern="[a-zA-ZÑñ\s]+"
+              required
+            />
+            {errors.pais && <ErrorMessage>{errors.pais}</ErrorMessage>}
+            <div style={{ margin: "15px" }} />
+
+            <ModalButtonGroup>
+              <button
+                type="button"
+                class="btn btn-success"
+                style={{ fontSize: "14px", padding: "10px 10px" }}
+                onClick={SaveModal}
+              >
+                Guardar
+              </button>
+              <button
+                type="button"
+                class="btn btn-secondary"
+                cancel
+                onClick={handleCloseModal}
+              >
+                Cancelar
+              </button>
+            </ModalButtonGroup>
+          </ModalWrapper>
+        </ModalBackground>
+      )}
+    </MainContainer>
+  );
+}
+
+export default Paises;
